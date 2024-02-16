5be6532c565e9aeaff2518f1ad650f15852da323
Apple101Ice <mercenaryxx@gmail.com>
2024-02-16 11:31:45 +0530

    ssl updated

--- dist/app.module.js
+++ dist/app.module.js
@@ -30,6 +30,9 @@ exports.AppModule = AppModule = __decorate([
                 username: process.env.PG_USERNAME,
                 password: process.env.PG_PASSWORD,
                 database: process.env.PG_DATABASE,
+                ssl: {
+                    rejectUnauthorized: false,
+                },
                 synchronize: true,
                 entities: [item_1.Item],
             }),
